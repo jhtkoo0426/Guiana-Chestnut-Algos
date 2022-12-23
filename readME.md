c1f4dec6b8ac676fa406661079522b1f461bf0f8
jhtkoo0426 <jhtkoo@gmail.com>
2022-12-23 21:49:17 +0000

    Added simple strategy for alpaca bot

--- readME.md
+++ readME.md
@@ -2,3 +2,6 @@
 Repository for potential ML features for the Guiana Chestnut project.
 
 Note: As of Dec 23, 2022, Yahoo Finance has updated the way they encode financial data, so the libraries yfinance and yahoo_fin have not been updated yet to tackle the change, hence some features do not work as of this code version.
+
+### Alpaca Bot
+- https://github.com/alpacahq/alpaca-py
--- src/alpaca_bot.py
+++ src/alpaca_bot.py
@@ -2,6 +2,10 @@ from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
+import yahoo_fin.stock_info as si
+import requests
+import time
+
 
 class AlpacaAPIConnection():
     """
@@ -10,11 +14,11 @@ class AlpacaAPIConnection():
 
     def __init__(self):
         # Initialize trading client for Alpaca API
-        API_KEY     = "PKKL587HXXS43RUJ8SFL"
-        SECRET_KEY  = "0mnSWoiLiSQsfUrgwOIMC8YpRQhIZhRSPwADyfR7"
+        API_KEY         = "PKKL587HXXS43RUJ8SFL"
+        SECRET_KEY      = "0mnSWoiLiSQsfUrgwOIMC8YpRQhIZhRSPwADyfR7"
         
-        self.client  = TradingClient(api_key=API_KEY, secret_key=SECRET_KEY, paper=True)
-        self.account = self.client.get_account()
+        self.client     = TradingClient(api_key=API_KEY, secret_key=SECRET_KEY, paper=True)
+        self.account    = self.client.get_account()
 
 
     # Account-related details
@@ -39,6 +43,12 @@ class AlpacaAPIConnection():
     def _get_account_short_market_value(self):
         return self.account.short_market_value
     
+
+    # Market-related functions
+    def _get_market_status(self):
+        clock = self.client.get_clock()
+        return clock.is_open
+
 
     # Trading-related functions
     def account_place_buy_order(self, symbol: str, quantity, type='market', time_in_force=TimeInForce.GTC):
@@ -72,26 +82,39 @@ class AlpacaAPIConnection():
         return None
 
 
-class StockScanner():
+class StockScanner(AlpacaAPIConnection):
     """
     Class responsible for processing realtime and historic stock data.
     """
 
     def __init__(self):
         pass
-    
 
-class Bot(AlpacaAPIConnection, StockScanner):
+
+class Bot(AlpacaAPIConnection):
     """
     Trading bot class to formulate strategies and make market orders.
     """
 
     def __init__(self):
         super().__init__()
+        self.watchlist = []
+        self.MAX_VALUE = 500
 
 
     def strategy(self):
-        pass
-
+        self.watchlist = si.tickers_sp500()
+        
+        while self._get_market_status() is True:
+            for ticker in self.watchlist:
+                # If have positions, don't buy extra
+                if ticker not in self._get_account_holdings().keys():
+                    ticker_price = 10
+                    self.account_place_buy_order(
+                        symbol=ticker,
+                        quantity=self.MAX_VALUE//ticker_price
+                    )
+            time.sleep(1800)
 
 bot     = Bot()
+print(bot._get_market_status())
